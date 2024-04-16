f9faaabc8fc4fdc45ab551947ed676e068cd1fc2
Süleyman ASLAN <suleymanaslan.8006@gmail.com>
2024-04-16 12:15:05 +0300

    Cart işlemleri reducers ve navbar gösterim işlemleri

--- src/components/detail/DetailCard.jsx
+++ src/components/detail/DetailCard.jsx
@@ -1,6 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useDispatch } from 'react-redux';
+import { addToCart } from '../../redux/cartSlice';
 
 const DetailCard = ({ data }) => {
+    const dispatch = useDispatch();
+    const [quantity, setQuantity] = useState(0)
+    const decrement = () => {
+        if (quantity > 0) {
+            setQuantity(quantity - 1)
+        }
+    }
+    const increment = () => {
+        if (quantity < data.rating?.count)
+            setQuantity(quantity + 1)
+    }
+    const addBasket = () => {
+        dispatch(addToCart({ id: data?.id, title: data?.title, price: data?.price, image: data?.image, quantity: quantity }));
+    }
     return (
         <div className='flex gap-10 my-10'>
             <img className="w-[700px] h-[500px] object-cover" src={data?.image} alt="" />
@@ -8,14 +24,14 @@ const DetailCard = ({ data }) => {
                 <div className='text-3xl font-bold'>{data?.title}</div>
                 <div className='my-2 text-2xl'>{data?.description}</div>
                 <div className='my-2 text-xl text-red-500'>Rating: {data?.rating?.rate}</div>
-                <div className='my-2 text-xl text-red-500'>Count: {data?.count?.rate}</div>
+                <div className='my-2 text-xl text-red-500'>Count: {data?.rating?.count}</div>
                 <div className='text-3xl font-bold'>{data?.price} <span className='text-sm'>TL</span></div>
                 <div className='flex items-center gap-5 my-4'>
-                    <div className='text-3xl '>-</div>
-                    <input className='w-[50px] text-center' type='text' value="0" />
-                    <div>+</div>
+                    <div onClick={decrement} className='text-5xl cursor-pointer'>-</div>
+                    <input className='w-12 text-center' type='text' value={quantity} />
+                    <div onClick={increment} className='text-5xl cursor-pointer'>+</div>
                 </div>
-                <div className='my-4 border w-[200px] text-2xl rounded-md bg-gray-200 cursor-pointer h-16 flex items-center justify-center'>Sepete Ekle</div>
+                <div onClick={addBasket} className='my-4 border w-[200px] text-2xl rounded-md bg-gray-200 cursor-pointer h-16 flex items-center justify-center'>Sepete Ekle</div>
             </div>
         </div>
     )
--- src/components/navbar/navbaritem/NavbarRight.jsx
+++ src/components/navbar/navbaritem/NavbarRight.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { BiSearch } from 'react-icons/bi';
 import { AiOutlineHeart } from 'react-icons/ai';
 import { SlBasket } from 'react-icons/sl';
+import { useDispatch, useSelector } from 'react-redux';
+import { getCartTotal } from '../../../redux/cartSlice';
 
 
 export const NavbarRight = () => {
+    const dispatch = useDispatch();
+    const { carts } = useSelector(state => state.carts);
+    console.log(carts);
+    useEffect(() => {
+        dispatch(getCartTotal())
+    }, [dispatch])
     return (
         <div className='flex items-center gap-8'>
             <div className='flex items-center rounded-full border p-3 bg-gray-200'>
@@ -13,7 +21,7 @@ export const NavbarRight = () => {
             </div>
             <AiOutlineHeart size={28} />
             <div className='relative'>
-                <div className='absolute -top-3 -right-3 w-5 h-5 bg-red-500 text-white rounded-full text-xs flex justify-center items-center'>3</div>
+                <div className='absolute -top-3 -right-3 w-5 h-5 bg-red-500 text-white rounded-full text-xs flex justify-center items-center'>{carts?.length}</div>
                 <SlBasket size={28} />
             </div>
         </div>
